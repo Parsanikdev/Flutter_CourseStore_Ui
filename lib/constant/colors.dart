21ad4f52e8a29297ffc526c06ae7febb2f236172
ParsaNik <parsa.niksirat83@gmail.com>
2025-01-14 22:08:28 +0330

    first commit

---
+++ lib/constant/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppgradiantColors {
+  static List<Color> appbarcolors = [
+    const Color.fromARGB(255, 132, 112, 233),
+    const Color.fromARGB(255, 102, 76, 231),
+  ];
+}
